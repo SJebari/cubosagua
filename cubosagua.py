d153deaec153f51a7da2e393662bebf6da77f5fb
Soulayman <soujebben@alu.edu.gva.es>
2024-05-22 21:16:02 +0200

    t1

--- cubosagua.py
+++ cubosagua.py
@@ -63,6 +63,7 @@ class juegoCubos:
         print('  (L)lenar un cubo')
         print('  (V)aciar un cubo')
         print('  (M)over el agua de un cubo a otro')
+        print('  (P)lenar todos los cubos')
         print('  (S)alir')
 
         while True:
@@ -71,7 +72,7 @@ class juegoCubos:
                 print('Gracias por jugar!')
                 sys.exit()
 
-            if move in ('L', 'V', 'M'):
+            if move in ('L', 'V', 'M', 'P'):
                 return move
 
     def selecCubo(self, mensaje):
@@ -104,6 +105,10 @@ class juegoCubos:
         self.aguaEnCubos[cuboDestino] += cantidadAMover
         self.pasos += 1
 
+    def llenarTodosCubos(self):
+        for cubo in self.aguaEnCubos:
+            self.llenarCubo(cubo)
+
     def jugar(self):
         self.mostrarEstadoCubos()
         while True:
@@ -118,6 +123,8 @@ class juegoCubos:
                 cuboOrigen = self.selecCubo('Selecciona el cubo ORIGEN 8, 5, 3 o SALIR:')
                 cuboDestino = self.selecCubo('Selecciona el cubo DESTINO 8, 5, 3 o SALIR:')
                 self.moverCubo(cuboOrigen, cuboDestino)
+            elif opcion == "P":
+                self.llenarTodosCubos()
             self.mostrarEstadoCubos()
             self.checkObjetivo()
 
